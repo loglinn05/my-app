9f88f1a5c8cc72d479cb55cf75fbfe9143b3e1cd
loglinn05 <loglinn05@gmail.com>
2023-03-21 23:29:12 +0200

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    handleClick () {
+        // alert("Oh, you've just pushed me!");
+        axios.get('https://api.github.com/users/cassidoo')
+            .then(response => {
+                // console.log(response);
+                this.setState({
+                    isLoaded: true,
+                    user: {
+                        avatar_url: response.data.avatar_url,
+                        login: response.data.login,
+                        name: response.data.name,
+                        bio: response.data.bio,
+                    }
+                });
+            });
+    }
+    constructor() {
+        super();
+        this.state = {
+            isLoaded: false,
+            user: {
+                avatar_url: '',
+                login: '',
+                name: '',
+                bio: ''
+            }
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    render () {
+        /*
+        let profile_container = (
+            <div className="profile-container">
+                <img src={this.state.user.avatar_url} alt="Avatar"/>
+                <h3>{this.state.user.name}</h3>
+                <h4>@{this.state.user.login}</h4>
+                <p>{this.state.user.bio}</p>
+            </div>
+        );
+        */
+
+        let profile_container = (
+            <Card border="danger" className="bootstrap-profile-container">
+                <Card.Img variant="top" src={this.state.user.avatar_url} />
+                <Card.Body>
+                    <Card.Title>{this.state.user.name}</Card.Title>
+                    <Card.Text>{this.state.user.bio}</Card.Text>
+                    <Card.Link href={'https://github.com/' + this.state.user.login}>
+                        @{this.state.user.login}
+                    </Card.Link>
+                </Card.Body>
+            </Card>
+        );
+
+        return (
+            <main>
+                <div className="button-container">
+                    <Button variant="outline-danger" className="bootstrap-button" onClick={this.handleClick}>Push me!</Button>
+                </div>
+                {this.state.isLoaded && profile_container}
+            </main>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Тут, в index.css, я буду задавати стилі.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+  Тут ми створюємо корінь застосунку, який потім використовуємо
+  для рендерингу компоненту App або іншого коду. Метод createRoot
+  описаний тут:
+  https://reactjs.org/docs/react-dom-client.html#createroot
+*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+root.render(
+    // <h1>React</h1>
+    React.createElement(
+        "h1",
+        null,
+        "React"
+    )
+);
+*/
 root.render(
   <React.StrictMode>
     <App />
